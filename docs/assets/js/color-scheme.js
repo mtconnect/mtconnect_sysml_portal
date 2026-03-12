f51f77beb2d8763e27db9f622f9d6a5ae9d3a5f1
Will Sobel <will@wvsobel.llc>
2026-03-12 21:07:06 +0100

    set the color scheme on load

--- docs/assets/js/color-scheme.js
+++ docs/assets/js/color-scheme.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Set initial theme
-  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    jtd.setTheme('dark');
-  } else {
-    jtd.setTheme('light');
-  }
+// Set initial theme
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  jtd.setTheme('dark');
+} else {
+  jtd.setTheme('light');
+}
 
+document.addEventListener('DOMContentLoaded', function () {
   // Listen for OS-level changes
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function (e) {
     jtd.setTheme(e.matches ? 'dark' : 'light');
